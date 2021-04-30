dd0ac5a1fa78d8b7d369f842016c72819e1398e9
mrGeist469 <75024897+mrGeist469@users.noreply.github.com>
2021-04-30 20:22:18 +0300

    Initializing the class

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ from pprint import pprint
 
 
 with open('token.txt', encoding='utf-8') as f:
-    token_vk = f.readline()
-    token_ya = f.readline()
+    token_vk = f.readline().strip()
+    token_ya = f.readline().strip()
 
 
 class User:
-    def __init__(self, user_id):
+    def __init__(self, user_id, token):
         self.user_id = user_id
+        self.token = token
 
-    def
+    def copy_photo(self):
+        url_vk = 'https://api.vk.com/method/photos.get'
+        params = {
+            'access_token': token_vk,
+            'owner_id': self.user_id,
+            'album_id': 'profile',
+            'extend': 1,
+            'count': 6,
+            'v': 5.130
+        }
+        result = requests.get(url_vk, params=params)
+
+
